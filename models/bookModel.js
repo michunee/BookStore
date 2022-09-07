65376e34cf0b823973fafa049bde4ba6f279c12f
Nguyen Khoi <khoicari69@gmail.com>
2022-09-07 10:10:42 +0700

    add new api book by category

--- models/bookModel.js
+++ models/bookModel.js
@@ -14,3 +14,17 @@ exports.getAllBooks = async()=>{
         })
     })
 }
+
+exports.getBookByCategory = async(catId)=>{
+    return new Promise((resolve, reject) => {
+        let sql = "SELECT * FROM book where catId = " + catId;
+        db.query(sql, (err, data) => {
+            if (err) console.log(err);
+            else
+            {
+                listData = data;
+                resolve(listData);
+            }
+        })
+    })
+}
