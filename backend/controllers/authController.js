92c49f9edf588eda71fccd3148dc8181cb2cb3f9
Minh Trí <81370854+PMinhTri@users.noreply.github.com>
2022-09-27 14:37:29 +0700

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,35 +1,115 @@
-const User = require('../models/userModel');
-const bcrypt = require('bcrypt');
+const User = require("../models/userModel");
+const argon2 = require("argon2");
+const jwt = require("jsonwebtoken");
 
-const createSendToken = (user, statusCode, res)=>{
-    res.status(statusCode).json({
-        status: 'success',
-        data : user
+const register = async (req, res) => {
+  const { username, password, birthname, email, phonenumber, address, admin } =
+    req.body;
+
+  // Check if user and passworld exist
+  if (!username || !password || !email) {
+    return res.status(400).json({
+      status: "fail",
+      message: "Please provide full information !",
     });
-}
+  }
+
+  // Check for user existing
+  try {
+    const user = await User.getUserByEmail(email);
+    console.log(user);
+    if (user === null) {
+      return res.status(400).json({
+        status: "fail",
+        message: "User already exists",
+      });
+    }
+
+    // All good
+    const hashedPassword = await argon2.hash(password);
+    const newUser = User.createUser(
+      username,
+      hashedPassword,
+      birthname,
+      email,
+      phonenumber,
+      address,
+      admin
+    );
+
+    //Return token
+    const accessToken = jwt.sign(
+      {
+        userId: newUser[0].id,
+        isAdmin: newUser[0].admin,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.status(200).json({
+      status: "success",
+      message: "User created successfully",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: "fail",
+      message: "Internal server error",
+      accessToken,
+    });
+  }
+};
 
 const login = async (req, res) => {
-    const { email, password } = req.body;
-    // 1) Check if email and password exist
-    if (!email || !password) {
-        return res.status(400).json({
-            status : "fail",
-            message : "Please provide username and password !"
-        });
-    }
-    // 2) Check if user exists && password is correct
+  const { email, password } = req.body;
+
+  // Check if user and passworld exist
+  if (!email || !password)
+    return res.status(400).json({
+      status: "fail",
+      message: "Please provide email and password!",
+    });
+
+  // Check for existing User
+  try {
     const user = await User.getUserByEmail(email);
-    const isEqual = await bcrypt.compare(password, user[0].password);
-    if (!user || !isEqual) {
-        return res.status(401).json({
-            status : "fail",
-            message : "Incorrect password !"
-        });
-    }
-    // 3) If everything ok, send token to client
-    createSendToken(user, 200, res);
-}
+    if (!user)
+      return res.status(400).json({
+        status: "fail",
+        message: "Incorrect username or password",
+      });
+
+    // User found
+    const passwordValid = await argon2.verify(user[0].password, password);
+    if (!passwordValid)
+      return res.status(400).json({
+        status: "fail",
+        message: "Incorrect username or password",
+      });
+    //All good
+    //Return token
+    const accessToken = jwt.sign(
+      {
+        userId: user[0].id,
+        isAdmin: user[0].admin,
+      },
+      process.env.JWT_SECRET
+    );
+    res.json({
+      status: "success",
+      message: "User logged in successfully",
+      accessToken,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: "fail",
+      message: "Internal server error",
+    });
+  }
+};
 
 module.exports = {
-    login
-}
+  login,
+  register,
+};
