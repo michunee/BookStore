eb1610f17ac46db100814b5beaca137553babe1f
Minh Trí <81370854+PMinhTri@users.noreply.github.com>
2022-09-27 14:41:09 +0700

    Add files via upload

---
+++ backend/middlewares/authorization.js
@@ -0,0 +1,23 @@
+const verifyToken = require("./verifyToken");
+
+const verifyUser = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not allowed to do that!");
+    }
+  });
+};
+
+const verifyAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not allowed to do that!");
+    }
+  });
+};
+
+module.exports = { verifyUser, verifyAdmin };
