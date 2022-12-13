c67b08867193a0983c99581f6331103e9f39fb70
Nguyen Khoi <khoi.nguyen@team.enouvo.com>
2022-12-13 21:49:43 +0700

    fix update user api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,31 +1,36 @@
 const User = require("../models/userModel");
 
 const getAllUser = async (req, res) => {
-    const user = await User.getAllUser();
-    res.status(200).json({
-        user: user,
-    });
+  const user = await User.getAllUser();
+  res.status(200).json({
+    user: user,
+  });
 };
 
 const getUserByUsername = async (req, res) => {
-    console.log(req.user);
-    const username = req.params.username;
-    const user = await User.getUserByUsername(username);
-    res.status(200).json({
-        user: user,
-    });
+  const username = req.params.username;
+  const user = await User.getUserByUsername(username);
+  res.status(200).json({
+    user: user,
+  });
 };
 
 const updateUserByUsername = async (req, res) => {
-    const username = req.params.username;
-    const user = await User.updateUserByUsername(username, req.body);
-    res.status(200).json({
-        message: "Update user success!",
-    });
+  const username = req.params.username;
+  const { phonenumber, address, birthname } = req.body;
+  const user = await User.updateUserByUsername(
+    username,
+    phonenumber,
+    address,
+    birthname
+  );
+  res.status(200).json({
+    message: "Update user success!",
+  });
 };
 
 module.exports = {
-    getAllUser,
-    getUserByUsername,
-    updateUserByUsername,
+  getAllUser,
+  getUserByUsername,
+  updateUserByUsername,
 };
