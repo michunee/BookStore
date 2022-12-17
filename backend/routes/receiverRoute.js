44a339ce336abe0b0617bd7a68824715b09f587b
Nguyen Khoi <khoicari69@gmail.com>
2022-12-17 16:23:01 +0700

    add new db and api receiver

--- backend/routes/receiverRoute.js
+++ backend/routes/receiverRoute.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const receiverController = require("../controllers/receiverController");
+const protect = require("../middlewares/authorization");
 
-router.route("/cart/:cartId").get(receiverController.getReceiverbyCartId);
+router
+  .route("/cart/:cartId")
+  .get(protect.verifyUser, receiverController.getReceiverbyCartId);
 
-router.route("/:username").post(receiverController.createReceiverbyUsername);
+router
+  .route("/:username")
+  .post(protect.verifyUser, receiverController.createReceiverbyUsername);
 
 module.exports = router;
