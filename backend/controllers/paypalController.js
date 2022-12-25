75ba42727d073d6c29c1183f1e7ee9bd062b4ceb
Nguyen Khoi <khoicari69@gmail.com>
2022-12-25 18:12:42 +0700

    add route and fix api paypal

--- backend/controllers/paypalController.js
+++ backend/controllers/paypalController.js
@@ -47,7 +47,9 @@ const createPayment = async (req, res) => {
 
   paypal.payment.create(create_payment_json, function (error, payment) {
     if (error) {
-      throw error;
+      res.status(400).json({
+        message: "Payment failed!",
+      });
     } else {
       for (let i = 0; i < payment.links.length; i++) {
         if (payment.links[i].rel === "approval_url") {
--- backend/routes/paypalRoute.js
+++ backend/routes/paypalRoute.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const paypalController = require("../controllers/paypalController");
+const protect = require("../middlewares/authorization");
 
-router.route("/:username").get(paypalController.createPayment);
+router.route("/").get(protect.verifyUser, paypalController.createPayment);
 
-router.route("/return/success").get(paypalController.successPayment);
+router
+  .route("/return/success")
+  .get(protect.verifyUser, paypalController.successPayment);
 
-router.route("/return/cancel").get(paypalController.cancelPayment);
+router
+  .route("/return/cancel")
+  .get(protect.verifyUser, paypalController.cancelPayment);
 
 module.exports = router;
