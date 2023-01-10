2bf190e62d6883f2f0fb370cd38bec98b3eff4b3
Nguyen Khoi <khoicari69@gmail.com>
2023-01-10 17:22:35 +0700

    test key

--- frontend/src/hooks/useAxios.js
+++ frontend/src/hooks/useAxios.js
@@ -1,29 +1,28 @@
-import axios from 'axios';
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 axios.defaults.baseURL = "http://localhost:3000/";
 
 const useAxios = ({ url }) => {
-    const [response, setResponse] = useState(null);
-    const [error, setError] = useState("");
-    const [loading, setLoading] = useState(true);
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await axios.get(url, {
-                headers: {
-                    'Authorization': `Bearer ${localStorage.getItem('token')}`
-                }
-            })
-            setResponse(response.data)
-            setLoading(false)
-            setError("")
-        }
-        fetchData();
-    }, [url]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      setResponse(response.data);
+      setLoading(false);
+      setError("");
+    };
+    fetchData();
+  }, [url]);
 
-    return { response, error, loading }
-}
+  return { response, error, loading };
+};
 
 export default useAxios;
-
